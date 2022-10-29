71f3ae731d63c3c0bcee33f26d8f0f8aae893f55
classic-k <classicconceptone@gmail.com>
2022-10-29 04:09:34 +0000

    CI Test

--- udagram_tests/git_test.sh
+++ udagram_tests/git_test.sh
@@ -10,15 +10,15 @@ brs3=main
 m1="fatal: a branch named '$brs1' already exists"
 m2="fatal: a branch named '$brs2' already exists"
 m3="fatal: a branch named '$brs3' already exists"
-if [[ "$(git checkout -b $brs1)" != "$m1" ]]; then
+if [[ "$(git checkout -b $brs1 2>&1)" != "$m1" ]]; then
     echo "inv $m1"
     exit 1
 fi
-if [[ "$(git checkout -b $brs2)" != "$m2" ]]; then
+if [[ "$(git checkout -b $brs2 2>&1)" != "$m2" ]]; then
     echo "inv2 $m2"
     exit 1
 fi
-if [[ "$(git checkout -b $brs3)" != "$m3" ]]; then
+if [[ "$(git checkout -b $brs3 2>&1)" != "$m3" ]]; then
     echo "inv3 $m3"
     exit 1
 fi
