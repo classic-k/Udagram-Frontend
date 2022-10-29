77a32f2a2bb2d47c33cfd8c6434a7a40be70cc48
classic-k <classicconceptone@gmail.com>
2022-10-29 03:41:28 +0000

    CI Test

--- udagram_tests/git_test.sh
+++ udagram_tests/git_test.sh
@@ -5,18 +5,18 @@
 #verify cannot push to staging or master (protected branches)
 
 brs=(dev staging main)
-m1="fatal: A branch named '$brs[1]' already exists."
+m1="fatal: A branch named $brs[1]' already exists."
 m2="fatal: A branch named '$brs[2]' already exists."
 m3="fatal: A branch named '$brs[3]' already exists."
-if [[ "$(git checkout -b $br[1])" != "$m1" ]]; then
+if [[ "$(git checkout -b $brs[1])" != "$m1" ]]; then
     echo "$m1"
     exit 1
 fi
-if [[ "$(git checkout -b $br[2])" != "$m2" ]]; then
+if [[ "$(git checkout -b $brs[2])" != "$m2" ]]; then
     echo "$m2"
     exit 1
 fi
-if [[ "$(git checkout -b $br[3])" != "$m3" ]]; then
+if [[ "$(git checkout -b $brs[3])" != "$m3" ]]; then
     echo "$m3"
     exit 1
 fi
