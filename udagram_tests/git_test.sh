3bb841ec6f7110fd159a23869a971d62261351fe
classic-k <classicconceptone@gmail.com>
2022-10-30 05:14:17 +0000

    Updates

--- udagram_tests/git_test.sh
+++ udagram_tests/git_test.sh
@@ -46,9 +46,9 @@ echo "Add updates to readme to test push" >> README.md
 git add README.md
 git commit -m "Test Push"
 #output=$(git push | grep -o 'Protected.*' 2>$1)
-output=$(echo `git push` 1>&2)
+output=$(git push 2>&1)
 err=$(echo "Protected branch update failed for refs/heads/$st")
-echo "output $output end"
+echo "$output"
 if [[ $output != $err ]]; then
     echo "Protection rule failed"
     exit 1
