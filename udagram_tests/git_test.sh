d003acafc768fede1ce84d65e39217ad8e0ad2e5
classic-k <88990430+classic-k@users.noreply.github.com>
2022-11-01 15:06:59 +0100

    Update git_test.sh

--- udagram_tests/git_test.sh
+++ udagram_tests/git_test.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 #@ TODO notes
-Verify dev, staging, and master exist
-verify working on feature branch or similar
-verify cannot push to staging or master (protected branches)
+#Verify dev, staging, and master exist
+#verify working on feature branch or similar
+#verify cannot push to staging or master (protected branches)
 
-brs=(dev staging main)
-m1=fatal: A branch named '$brs[1]' already exists.
-m2=fatal: A branch named '$brs[2]' already exists.
-m3=fatal: A branch named '$brs[3]' already exists.
-if [[ "$(git checkout -b $br[1])" != "$m1" ]]; then
-    echo "$m1"
-    exit 1
-fi
-if [[ "$(git checkout -b $br[2])" != "$m2" ]]; then
-    echo "$m2"
-    exit 1
-fi
-if [[ "$(git checkout -b $br[3])" != "$m3" ]]; then
-    echo "$m3"
-    exit 1
-fi
-cbr=$(${GITHUB_REF#refs/heads/})
-echo $cbr
-echo "##[set-output name=branch;]$(echo ${GITHUB_REF#refs/heads/})"
+
